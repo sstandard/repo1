aeb62c4f6d1d2d4da39ca753b764515ad7c7b477
stephen standard <sstandard@yahoo.com>
2025-06-22 21:12:29 +0100

    combine 2 messages

---
+++ Python/python1.py
@@ -0,0 +1,18 @@
+python_message = ('hello world')
+#print message
+print(python_message)
+#
+# declare first name last name
+first_name = "ste"
+second_name = "standard"
+full_name = first_name + " " + second_name
+#
+# Function - title : Capitalise the Surname
+message = "Hello, " + full_name.title() + "!"
+print(message)
+print(3*100)
+#
+# Function - convert a number to a string.
+age = 54
+age_message = "You are " + str(age) + " years old"
+print(message + " " + age_message)
